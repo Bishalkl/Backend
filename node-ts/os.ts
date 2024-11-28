9f5b1655e4dab657b89440a8db0a361af1a7d06f
Bishalkl <Bishal123koirala@gmail.com>
2024-11-28 17:24:45 +0545

    done

--- node-ts/os.ts
+++
@@ -1,5 +0,0 @@
-import * as os from "os";
-
-var totalMemory: number = os.totalmem();
-var freeMemory: number = os.freemem();
-console.log(`Total Memory is ${totalMemory} and freeMemory is ${freeMemory}`);
--- node-ts/path.ts
+++
@@ -1,4 +0,0 @@
-import * as path from "path";
-
-const pathobj: path.ParsedPath = path.parse(__filename);
-console.log(pathobj);
